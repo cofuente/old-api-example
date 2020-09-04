4420e20c67e1e08a9f170a51f61cef03768eb99f
cofuente <contact@cofuente.io>
2020-09-04 12:26:46 -0400

    gave a fixed UUID to match seed
    Signed-off-by: cofuente <contact@cofuente.io>

--- client/utils/data.js
+++ client/utils/data.js
@@ -1,13 +1,6 @@
 var axios = require('axios');
 const current = process.env.NODE !== '/app/.heroku/node/bin/node' ? 'http://localhost:1337' : 'https://secure-form-api.herokuapp.com'
-let formUUID;
-
-if (current === "http://localhost:1337") {
-  formUUID = "78d51d9e-0285-4022-81c5-7f14955315d0"
-} else {
-  formUUID = "eeae34c5-de07-4e1a-8402-602f158ca0a1"
-}
-
+const formUUID ="78d51d9e-0285-4022-81c5-7f14955315d0"
 
 module.exports.data = axios.get(`${current}/api/forms/${formUUID}`)
   .then(response => {
