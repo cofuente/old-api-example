2fefdeb44765d3b44585c2ca7c28ffcd81921d16
cofuente <contact@cofuente.io>
2020-09-04 07:50:58 -0400

    fake testing file not working
    Signed-off-by: cofuente <contact@cofuente.io>

--- server/api/form.spec.js
+++ server/api/form.spec.js
@@ -1,7 +1,7 @@
 const {expect} = require('chai')
 const request = require('supertest')
 const db = require('../db/_db')
-const app = require('../index')
+const {fullstack} = require('../../index')
 const Form = db.model('form')
 
 describe('Form routes', () => {
@@ -10,20 +10,23 @@ describe('Form routes', () => {
   })
 
   describe('/api/forms/', () => {
-    // const whateverForm = 'whatever'
+    const whateverForm = {
+        formUUID: 'b52e2f5f-763b-4e63-9124-0e707ea73f61',
+        title: 'example form title 1',
+        stateCode: 'NY',
+      }
 
     beforeEach(() => {
-      return Form.create({
-        // form: whateverForm
-      })
+      return Form.create(whateverForm)
     })
 
     it('GET /api/forms', async () => {
-      const res = await request(app)
+      const res = await request(fullstack)
         .get('/api/forms')
         .expect(200)
 
       expect(res.body).to.be.an('array')
+      console.log(res.body)
       // expect(res.body[0].email).to.be.equal(whateverForm)
     })
   }) // end describe('/api/forms')
