5b8ebc3c1b52d79654b75acaa8eba017e05556c2
cofuente <contact@cofuente.io>
2020-09-23 17:36:45 -0400

    ran auto linter
    Signed-off-by: cofuente <contact@cofuente.io>

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ const chalkAnimation = require('chalk-animation')
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
+
 const PORT = process.env.PORT || 9000
 const frontend = new express()
 const nextDistroEnrollmentForm = require('./utils/data.js')
@@ -11,19 +12,19 @@ frontend.use(cors())
 frontend.use(express.json())
 frontend.use(express.urlencoded({ extended: true }))
 
-// Serve Jade files 
+// Serve Jade files
 frontend.use(express.static(path.join(__dirname, './src/jade/next-distro-fe/')))
   .set('views', path.join(__dirname, './src/jade/next-distro-fe/'))
   .set('view engine', 'jade')
-  .get('/', function (req, res) {
-    nextDistroEnrollmentForm.data.then(data=>{
-      res.status(200).render('index', {data})
-    }).catch(e =>{
+  .get('/', (req, res) => {
+    nextDistroEnrollmentForm.data.then((data) => {
+      res.status(200).render('index', { data })
+    }).catch((e) => {
       console.log(e)
     })
   })
   .listen(PORT, () => {
-    setTimeout(() => chalkAnimation.pulse(`Web client served on port ${PORT}`), 3580) 
+    setTimeout(() => chalkAnimation.pulse(`Web client served on port ${PORT}`), 3580)
   })
 
 // Serve static files
