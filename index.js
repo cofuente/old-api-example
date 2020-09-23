bfec5605a658238ab1eb5206b5fe843af1f45477
cofuente <contact@cofuente.io>
2020-09-23 17:23:26 -0400

    ran auto linter
    Signed-off-by: cofuente <contact@cofuente.io>

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV !== 'production') require('dotenv').config()  // may have to update this to a different heroku specific env
+if (process.env.NODE_ENV !== 'production') require('dotenv').config() // may have to update this to a different heroku specific env
 const path = require('path')
 const chalk = require('chalk')
 const bodyParser = require('body-parser')
@@ -6,46 +6,45 @@ const express = require('express')
 const session = require('express-session')
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 const { db } = require('./server/db/models')
-const {customOptions, logger} = require('./server/utils')
-const storeOptions = customOptions(db) 
+const { customOptions, logger } = require('./server/utils')
+
+const storeOptions = customOptions(db)
 const sessionStore = new SequelizeStore(storeOptions)
 const PORT = process.env.PORT || 1337
 const current = process.env.NODE !== '/app/.heroku/node/bin/node' ? `http://localhost:${PORT}` : 'https://secure-form-api.herokuapp.com'
 const fullStack = express()
 
-
 const buildStack = async () => {
-
-  // session middleware 
+  // session middleware
   fullStack.use(
     session({
       secret: process.env.SESSIONS_SECRET || 'abc123',
       name: process.env.SECRET_SESSION_NAME || 'nextharm',
-      cookie: { 
+      cookie: {
         secure: true,
-        maxAge: 30 * 60 * 1000 // The maximum age (in milliseconds) of a valid session.
+        maxAge: 30 * 60 * 1000, // The maximum age (in milliseconds) of a valid session.
       },
       store: sessionStore,
       resave: true,
-      saveUninitialized: true
-    })
+      saveUninitialized: true,
+    }),
   )
-  
+
   // logging middleware
   fullStack.use(logger)
 
   // body parsing middleware
   fullStack.use(bodyParser.json())
-  fullStack.use(bodyParser.urlencoded({extended: true}))
+  fullStack.use(bodyParser.urlencoded({ extended: true }))
 
   // api routes
   fullStack.use('/api', require('./server/api'))
-  
-  // serve Jade files 
+
+  // serve Jade files
   fullStack.use(express.static(path.join(__dirname, './client/src/jade/next-distro-fe/')))
   fullStack.set('views', path.join(__dirname, './client/src/jade/next-distro-fe/'))
   fullStack.set('view engine', 'jade')
-  
+
   // serve static files
   fullStack.use('/', express.static('./client/src/'))
 
@@ -74,12 +73,12 @@ const serveClient = async () => {
   const nextDistroEnrollmentForm = require('./client/utils/data.js')
   try {
     await fullStack.get('/', (req, res) => {
-      nextDistroEnrollmentForm.data.then( (data) => res.status(200).render('index', {data}))
+      nextDistroEnrollmentForm.data.then((data) => res.status(200).render('index', { data }))
     })
-      .post('/', function(req){
-        var data = req.body
+      .post('/', (req) => {
+        const data = req.body
         postFormattedData(data)
-      })    
+      })
     console.log(chalk.magenta(`Client awaits at ${current}`))
   } catch (err) {
     console.error(err)
