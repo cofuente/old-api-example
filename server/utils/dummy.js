2ec1455b8eb6cf93bd2d7487947dc184d3467e63
cofuente <contact@cofuente.io>
2020-11-11 15:54:15 -0500

    linted
    Signed-off-by: cofuente <contact@cofuente.io>

--- server/utils/dummy.js
+++ server/utils/dummy.js
@@ -1,15 +1,16 @@
+/* eslint-disable no-console */
 const fs = require('fs')
 const path = require('path')
 
 const valueParser = (x) => {
-  if (x.questionType == 'paragraph') return 'this is a standard paragraph answer'
-  if (x.questionType == 'input') return 'this is a standard input answer'
-  if (x.questionType == 'checkbox' && x.possibleAnswers.length) return x.possibleAnswers[0][0]
-  if (x.questionType == 'date') return '2020-08-11'
-  if (x.questionType == 'dropdown' && x.possibleAnswers.length) return x.possibleAnswers[0][0]
-  if (x.questionType == 'radio' && x.possibleAnswers.length) return x.possibleAnswers[0][0]
-  if (x.questionType == 'heading') return ""
-  if (x.questionType == 'number') return "90210"
+  if (x.questionType === 'paragraph') return 'this is a standard paragraph answer'
+  if (x.questionType === 'input') return 'this is a standard input answer'
+  if (x.questionType === 'checkbox' && x.possibleAnswers.length) return x.possibleAnswers[0][0]
+  if (x.questionType === 'date') return '2020-08-11'
+  if (x.questionType === 'dropdown' && x.possibleAnswers.length) return x.possibleAnswers[0][0]
+  if (x.questionType === 'radio' && x.possibleAnswers.length) return x.possibleAnswers[0][0]
+  if (x.questionType === 'heading') return ''
+  if (x.questionType === 'number') return '90210'
 } 
 
 // create a json to use with postman
@@ -25,7 +26,7 @@ const dummySubmission = async (formUUID, questions) => {
     answers: parsedQuestions
   }
   await fs.writeFile(path.join('dummy-submission.json'), JSON.stringify(submissionData), (err) => {
-    if (err) throw err;
+    if (err) throw err
     console.log('The file has been saved!')
   })
 }
