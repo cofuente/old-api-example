d99eef2c5062ff6263b6f42a35c1566688165464
cofuente <contact@cofuente.io>
2020-11-30 14:27:28 -0500

    cleaning up
    Signed-off-by: cofuente <contact@cofuente.io>

--- server/api/index.js
+++ server/api/index.js
@@ -1,11 +1,9 @@
 
 const router = require('express').Router()
 
-// Ultimately, I forsee the client only needing endpoints to GET forms or POST submissions
+// Ultimately, I forsee the browser client only needing endpoints to GET forms or POST submissions
 router.use('/forms', require('./form'))
 router.use('/submissions', require('./submission'))
-
-// If we create some kind of admin-access client for this, we'll certainly need more endpoints
 router.use('/questions', require('./question'))
 
 
