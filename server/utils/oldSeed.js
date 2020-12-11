74ddc409b83439fc76c922573a074ff746f01c29
cofuente <contact@cofuente.io>
2020-12-10 19:48:39 -0500

    running old seed with just one form
    Signed-off-by: cofuente <contact@cofuente.io>

--- server/utils/oldSeed.js
+++ server/utils/oldSeed.js
@@ -10,7 +10,7 @@ const {
 
 } = require('../db/models')
 const distroEnrollmentData = require( './distroEnrollment' )
-const reverseReportData = require('./reverseReport')
+// const reverseReportData = require('./reverseReport')
 
 
 async function seed() {
@@ -20,9 +20,9 @@ async function seed() {
   const distroQuestions = await Promise.all(distroQuestionsArr)
   const distroQuestionUUIDs = distroQuestions.map(x => x.dataValues.questionUUID)
 
-  const reverseReportQuestionsArr = reverseReportData.map((x)=>Question.create(x))
-  const reverseReportQuestions = await Promise.all(reverseReportQuestionsArr)
-  const reverseReportQuestionUUIDs = reverseReportQuestions.map(x => x.dataValues.questionUUID)
+  // const reverseReportQuestionsArr = reverseReportData.map((x)=>Question.create(x))
+  // const reverseReportQuestions = await Promise.all(reverseReportQuestionsArr)
+  // const reverseReportQuestionUUIDs = reverseReportQuestions.map(x => x.dataValues.questionUUID)
 
   const forms = await Promise.all([
     Form.create({
@@ -34,19 +34,18 @@ async function seed() {
     }
     ).then(
       (form => form.addQuestions(distroQuestionUUIDs))
-    ),
-
-    Form.create({
-      formUUID: '78d51d9e-0285-4022-81c5-7f14955315d1',
-      title: 'Opioid Overdose Incident Form',
-      instructions: '<p>If you\'ve responded to an opioid overdose using the naloxone sent to you by our site or an affiliate organization, filling out this opioid overdose incident form is a necessity. Your responses will help us illustrate the importance of online and mail-based naloxone distribution. It is also the first step in mailing you a refill.</p><ul><li>Your name is not required.</li><li>If you responded to more than one overdose (more than one person, date, or location) please fill out one form for each occurrence.</li></ul>',
-      stateCode: 'NY'
-    }, {
-      include: [ Question ]
-    }
-    ).then(
-      (form => form.addQuestions(reverseReportQuestionUUIDs))
     )
+    // Form.create({
+    //   formUUID: '78d51d9e-0285-4022-81c5-7f14955315d1',
+    //   title: 'Opioid Overdose Incident Form',
+    //   instructions: '<p>If you\'ve responded to an opioid overdose using the naloxone sent to you by our site or an affiliate organization, filling out this opioid overdose incident form is a necessity. Your responses will help us illustrate the importance of online and mail-based naloxone distribution. It is also the first step in mailing you a refill.</p><ul><li>Your name is not required.</li><li>If you responded to more than one overdose (more than one person, date, or location) please fill out one form for each occurrence.</li></ul>',
+    //   stateCode: 'NY'
+    // }, {
+    //   include: [ Question ]
+    // }
+    // ).then(
+    //   (form => form.addQuestions(reverseReportQuestionUUIDs))
+    // )
   ])
 
   console.log(`seeded ${forms.length} forms`)
