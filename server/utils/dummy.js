a6b93e3801d738998d1573bd34c3f8ac1daf9bab
cofuente <contact@cofuente.io>
2020-12-10 19:38:02 -0500

    renamed seeds and dummies
    Signed-off-by: cofuente <contact@cofuente.io>

--- server/utils/dummy.js
+++ server/utils/dummy.js
@@ -1,34 +1,62 @@
 /* eslint-disable no-console */
 const fs = require('fs')
 const path = require('path')
+const { Form, Question } = require( '../db/models' )
 
-// eslint-disable-next-line consistent-return
-const valueParser = (x) => {
+// returns a standardized answer for every question type that does not have a possibleAnswers array
+const standardAnswers = (x) => {
   if (x.questionType === 'paragraph') return 'this is a standard paragraph answer'
   if (x.questionType === 'input') return 'this is a standard input answer'
-  if (x.questionType === 'checkbox' && x.possibleAnswers.length) return x.possibleAnswers[0][0]
   if (x.questionType === 'date') return '2020-08-11'
-  if (x.questionType === 'dropdown' && x.possibleAnswers.length) return x.possibleAnswers[0][0]
-  if (x.questionType === 'radio' && x.possibleAnswers.length) return x.possibleAnswers[0][0]
-  if (x.questionType === 'heading') return ''
+  if (x.questionType === 'heading') return 'this is a standard heading answer'
   if (x.questionType === 'number') return '90210'
+  if (x.questionType === 'textarea') return 'this is a standard textarea answer'
 } 
-
-// create a json to use with postman
-const dummySubmission = async (formUUID, questions) => {
-  const parsedQuestions = questions.map((x) => ({
-    questionUUID: x.questionUUID,
-    value: valueParser(x)
-  }))
-  const submissionData = {
-    formUUID,
-    answers: parsedQuestions,
-    stateCode: 'NY',
-    title: 'Secure Enrollment for NEXT Distro\'s Mail-based Harm Reduction Program'
-  }
-  await fs.writeFile(path.join('dummy-submission.json'), JSON.stringify(submissionData), (err) => {
-    if (err) throw err
-    console.log('The file has been saved!')
-  })
+// selects a random option from the question's possibleAnswers array or uses a standard answer as defined above
+const generateAnswer = (requiredQ) => {
+  const { possibleAnswers } = requiredQ
+  if (possibleAnswers.length > 1 || possibleAnswers[0] !== 'n/a') {
+    return possibleAnswers[Math.floor(Math.random() * (possibleAnswers.length))]
+  } 
+  return standardAnswers(requiredQ)
 }
-module.exports = dummySubmission
+
+// takes a complete form and generates complete dummy submission data
+const generateFromQuestions = ( completeForm ) => {
+  const {formUUID} = completeForm
+  const requiredQuestionsOnly = completeForm.questions.filter(x => x.required === true)
+  const answers = requiredQuestionsOnly.map(x => ({
+    questionUUID: x.questionUUID,
+    value: generateAnswer(x)
+  }))
+  return {
+    formUUID,
+    answers
+  }
+}
+
+
+// Generates the dummy-submission.js files
+const generateSubmissions = async () => {
+  const existingForms = await Form.findAll()
+  const formUUIDs = existingForms.map( x => x.formUUID )
+  const getCompleteForm = async (formUUID) =>  Form.findOne({
+    where: { formUUID },
+    include: {
+      model: Question,
+      as: 'questions'
+    }
+  })
+  const completeForms = await Promise.all(formUUIDs.map(getCompleteForm))
+  const submissionData = completeForms.filter(x =>x.questions.length).map(generateFromQuestions)
+  
+  const dummySubmissions = await Promise.all(submissionData.map( async (submission, i) => {
+    await fs.writeFile(path.join(`dummy-submission-${i}.json`), JSON.stringify(submission), (err) => {
+      if (err) throw err
+      console.log('A new submission has been generated!')
+    })
+  }))
+  return dummySubmissions
+}
+  
+generateSubmissions()
---
+++ server/utils/oldSeed.js
@@ -0,0 +1,83 @@
+/* eslint-disable max-len */
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
+// eslint-disable-next-line global-require
+if (process.env.NODE !== '/app/.heroku/node/bin/node') require('dotenv').config()
+const db = require('../db/_db')
+const {
+  Form,
+  Question
+
+} = require('../db/models')
+const distroEnrollmentData = require( './distroEnrollment' )
+const reverseReportData = require('./reverseReport')
+
+
+async function seed() {
+  await db.sync({ force: true })
+  console.log('db synced!')
+  const distroQuestionsArr = distroEnrollmentData.map((x)=>Question.create(x))
+  const distroQuestions = await Promise.all(distroQuestionsArr)
+  const distroQuestionUUIDs = distroQuestions.map(x => x.dataValues.questionUUID)
+
+  const reverseReportQuestionsArr = reverseReportData.map((x)=>Question.create(x))
+  const reverseReportQuestions = await Promise.all(reverseReportQuestionsArr)
+  const reverseReportQuestionUUIDs = reverseReportQuestions.map(x => x.dataValues.questionUUID)
+
+  const forms = await Promise.all([
+    Form.create({
+      formUUID: '78d51d9e-0285-4022-81c5-7f14955315d0',
+      title: 'Secure Enrollment for NEXT Distro\'s Mail-based Harm Reduction Program',
+      stateCode: 'NY'
+    }, {
+      include: [ Question ]
+    }
+    ).then(
+      (form => form.addQuestions(distroQuestionUUIDs))
+    ),
+
+    Form.create({
+      formUUID: '78d51d9e-0285-4022-81c5-7f14955315d1',
+      title: 'Opioid Overdose Incident Form',
+      instructions: '<p>If you\'ve responded to an opioid overdose using the naloxone sent to you by our site or an affiliate organization, filling out this opioid overdose incident form is a necessity. Your responses will help us illustrate the importance of online and mail-based naloxone distribution. It is also the first step in mailing you a refill.</p><ul><li>Your name is not required.</li><li>If you responded to more than one overdose (more than one person, date, or location) please fill out one form for each occurrence.</li></ul>',
+      stateCode: 'NY'
+    }, {
+      include: [ Question ]
+    }
+    ).then(
+      (form => form.addQuestions(reverseReportQuestionUUIDs))
+    )
+  ])
+
+  console.log(`seeded ${forms.length} forms`)
+  console.log(`with ${distroQuestionUUIDs.length} distroQuestions`)
+  console.log('seeded successfully')
+}
+
+// Separated the `seed` function from the `runSeed` function.
+// This way we can isolate the error handling and exit trapping.
+// The `seed` function is concerned only with modifying the database.
+async function runSeed() {
+  console.log('🌱 running seed')
+  try {
+    await seed()
+  } catch (err) {
+    console.error(err)
+    process.exitCode = 1
+  } finally {
+    console.log('closing db connection')
+    await db.close()
+    console.log('db connection closed')
+  }
+}
+
+// Execute the `seed` function, IF we ran this module directly (`node seed`).
+// `Async` functions always return a promise, so we can use `catch` to handle
+// any errors that might occur inside of `seed`.
+
+if (module === require.main) {
+  runSeed()
+}
+
+// Exporting the seed function for testing purposes
+module.exports = seed
