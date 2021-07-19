411f17116ffe2eb124682b016ce4300e2b7f5b53
Bonga <bonga.developer@outlook.com>
2021-07-19 21:23:54 +0200

    inital stages

--- server/api/user/user.js
+++ server/api/user/user.js
@@ -2,5 +2,15 @@ const router = require('express').Router()
 const db = require('../../config/db')
 
 router.post('/login', async (req, res) => {
-    
+    try {
+        const params = req.params
+        const submission = Submission.findAll({
+            where: {
+              Id: params
+            }
+          })
+        res.send( submission )
+    } catch ( err ) {
+        console.log('Error Occured:  ', err)
+    }
 }
