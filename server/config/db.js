faab20c3fee2c69fd29b68cf4a97f5a6caa70dd7
Chiara Marcial Martínez <contact@cofuente.io>
2021-07-21 14:40:37 -0400

    refactor
    Signed-off-by: Chiara Marcial Martínez <contact@cofuente.io>

--- server/config/db.js
+++ server/config/db.js
@@ -3,22 +3,14 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
-let config
-
-if (process.env.DATABASE_URL) {
-  config = {
-    logging: false,
+const config = {
+  logging: !process.env.DATABASE_URL,
     ssl: true,
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false
       }
-    }
-  }
-} else {
-  config = {
-    logging: false
   }
 }
 
