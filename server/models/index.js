b34643f4bbc7923f75e2d92d469c5685023f395f
Chiara Marcial Martínez <contact@cofuente.io>
2021-07-21 15:28:51 -0400

    rearranged order
    Signed-off-by: Chiara Marcial Martínez <contact@cofuente.io>

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,17 @@
-const User = require( './user' )
 const Program = require('./program')
-const Question = require('./question')
-const Answer = require( './answer' )
-const Client = require('./client')
 const Form = require('./form')
-const Submission = require('./submission')
+const Client = require('./client')
+const User = require( './user' )
+const Question = require('./question')
 const QuestionForm = require('./questionform')
+const Submission = require('./submission')
+const Answer = require( './answer' )
 
+Form.belongsTo( Program, {foreignKey: 'programUUID'} )
+
+Client.belongsTo( Program, {foreignKey: 'programUUID'} )
+
+User.belongsTo(Program, {foreignKey: 'programUUID'})
 
 Question.belongsToMany(Form, {
   through: QuestionForm,
@@ -19,23 +24,17 @@ Form.belongsToMany(Question, {
 
 Submission.belongsTo(Form, {foreignKey: 'formUUID'})
 
-Form.belongsTo( Program, {foreignKey: 'programUUID'} )
-
-Client.belongsTo( Program, {foreignKey: 'programUUID'} )
-
-User.belongsTo(Program, {foreignKey: 'programUUID'})
-
 Answer.belongsTo(Question, {foreignKey: 'questionUUID'} )
 
 Submission.hasMany(Answer, {foreignKey: 'submissionUUID'})
 
 module.exports = {
-  Question,
-  Answer,
-  Form,
-  Submission,
-  QuestionForm,
   Program,
+  Form,
   Client,
-  User
+  User,
+  Question,
+  QuestionForm,
+  Submission,
+  Answer,
 }
