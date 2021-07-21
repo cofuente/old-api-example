73b4a91086561a60a9b7b59b16e437ca0c527573
Chiara Marcial Martínez <contact@cofuente.io>
2021-07-21 13:17:29 -0400

    mid-debugging, BROKEN
    Signed-off-by: Chiara Marcial Martínez <contact@cofuente.io>

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 
-router.use('/forms', require('./forms/formController'))
+router.use('/forms', require('./forms'))
+router.use('/users', require('./users'))
 
 module.exports = router
