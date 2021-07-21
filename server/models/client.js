7aa23b3938d22c79149c530bc65df77862ed690a
Chiara Marcial Martínez <contact@cofuente.io>
2021-07-21 15:03:36 -0400

    cleaned up
    Signed-off-by: Chiara Marcial Martínez <contact@cofuente.io>

--- server/models/client.js
+++ server/models/client.js
@@ -1,15 +1,13 @@
-const crypto = require('crypto')
 const Sequelize = require('sequelize')
 const db = require('../config/db')
 
-const Client = db.define( 'client', {
+const Client = db.define('client', {
   clientUUID: {
     primaryKey: true,
     type: Sequelize.UUID,
     defaultValue: Sequelize.UUIDV4,
     unique: true
   },
-  // TO CONSIDER: if a client belongs to a program we may not have a way to move the client from program to program
     programUUID: {
     type: Sequelize.UUID,
     references: {
@@ -18,53 +16,11 @@ const Client = db.define( 'client', {
     },
     allowNull: false
   },
-  // TO CONSIDER: unclear as to which would be the better paradigm
   handle: {
     type: Sequelize.STRING,
     unique: true,
     allowNull: false
   },
-  // handle: {
-  //   type: Sequelize.STRING,
-  //   get() {
-  //     return () => this.getDataValue('handle')
-  //   }
-  // },
-  // salt: {
-  //   type: Sequelize.STRING,
-  //   get() {
-  //     return () => this.getDataValue('salt')
-  //   }
-  // }
 })
 
 module.exports = Client
-// Client.prototype.correctHandle = function(candidatePwd) {
-//   return Client.encryptHandle(candidatePwd, this.salt()) === this.handle()
-// }
-
-// Client.generateSalt = function() {
-//   return crypto.randomBytes(16).toString('base64')
-// }
-
-// Client.encryptHandle = function(plainText, salt) {
-//   return crypto
-//     .createHash('RSA-SHA256')
-//     .update(plainText)
-//     .update(salt)
-//     .digest('hex')
-// }
-
-// const setSaltAndHandle = client => {
-//   const localClient = client
-//   if (localClient.changed('handle')) {
-//     localClient.salt = Client.generateSalt()
-//     localClient.handle = Client.encryptHandle(localClient.handle(), localClient.salt())
-//   }
-// }
-
-// Client.beforeCreate(setSaltAndHandle)
-// Client.beforeUpdate(setSaltAndHandle)
-// Client.beforeBulkCreate(users => {
-//   users.forEach(setSaltAndHandle)
-// })
