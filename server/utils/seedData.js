aab992600c2be24b8511ad94c457ab72df7cfb83
Chiara Marcial Martínez <contact@cofuente.io>
2021-07-23 00:36:08 -0400

    update question shape
    Signed-off-by: Chiara Marcial Martínez <contact@cofuente.io>

--- server/utils/seedData.js
+++ server/utils/seedData.js
@@ -3,76 +3,76 @@ const testData = {
     {
       questionUUID: 'ede83751-33f3-4dcf-8c12-b7fc650c501c',
       required: true,
+      encryptAnswer: false,
       questionPrompt: 'This is the question prompt.',
       questionType: 'multiplechoice',
-      questionDescription: 'This is the question description.',
       possibleAnswers: [ 'answer a', 'answer b', 'answer c' ]
     },
     {
       questionUUID: 'b2d224a6-0bf9-4c5b-880d-c62fec019a8d',
       required: true,
+      encryptAnswer: false,
       questionPrompt: 'This is the question prompt.',
       questionType: 'checkbox',
-      questionDescription: 'This is the question description.',
       possibleAnswers: [ 'checkbox option a', 'checkbox option b' ]
     },
     {
       questionUUID: '886d73ab-1a54-4616-b1fd-f4c4898ed059',
       required: true,
+      encryptAnswer: false,
       questionPrompt: 'This is the question prompt.',
       questionType: 'paragraph',
-      questionDescription: 'This is the question description.'
     },
     {
       questionUUID: '839743bd-fd80-4094-9d89-52fbee80dfcd',
       required: true,
+      encryptAnswer: false,
       questionPrompt: 'This is the question prompt.',
       questionType: 'input',
-      questionDescription: 'This is the question description.'
     },
     {
       questionUUID: '6b57b827-9694-4fb4-aaac-d265f86a2f72',
       required: true,
+      encryptAnswer: false,
       questionPrompt: 'This is the question prompt.',
       questionType: 'date',
-      questionDescription: 'This is the question description.'
     },
     {
       questionUUID: '3412308f-e653-4712-a1bd-abfd3d777ea5',
       required: true,
+      encryptAnswer: false,
       questionPrompt: 'This is the question prompt.',
       questionType: 'radio',
-      questionDescription: 'This is the question description.',
       possibleAnswers: [ 'radio option a', 'radio option b' ]
     },
     {
       questionUUID: '2ebb82be-a117-491c-bf52-64f57e358475',
       required: true,
+      encryptAnswer: false,
       questionPrompt: 'This is the question prompt.',
       questionType: 'dropdown',
-      questionDescription: 'This is the question description.',
       possibleAnswers: [ 'dropdown option a', 'dropdown option b' ]
     },
     {
       questionUUID: '24bafe55-5478-4917-882b-8e14348ec1d3',
       required: true,
+      encryptAnswer: false,
       questionPrompt: 'This is the question prompt.',
       questionType: 'heading',
-      questionDescription: 'This is the question description.'
     },
     {
       questionUUID: '21154dc7-5fac-43f9-a058-a4ee492cb304',
       required: true,
+      encryptAnswer: false,
       questionPrompt: 'This is the question prompt.',
       questionType: 'number',
-      questionDescription: 'This is the question description.'
     },
     {
       questionUUID: '0e956a4e-63a8-32ff-9f80-01daa32e1615',
       required: true,
+      encryptAnswer: false,
       questionPrompt: 'This is the question prompt.',
       questionType: 'textarea',
-      questionDescription: 'This is the question description.'
     }
   ],
   programs: [
@@ -98,13 +98,20 @@ const testData = {
       formUUID: '95dfc8ea-30a1-4c4b-be03-62fbf5b7cd2f',
       programUUID: '21154dc7-5fac-41f9-a058-a4ee492cb304',
       stateCode: 'NY',
-      title: 'Example of a form title.',
-      instructions: 'Here go example instructions.'
+      title: 'Secure Enrollment for NEXT Distro\'s Mail-based Harm Reduction Program',
+      instructions: 'We are committed to your safety and security, for that reason this enrollment form does not ask for your name and address. After you complete this form, please text the \"handle\" you choose and your name and mailing address to nextdistro@pm.me. Alternately you can use the Signal app (signal.org) to message (646) 389-0752. More information about Signal can be found at nextdistro.org/privacy Please reach out to Jamie and Dan at nextdistro@pm.me if you have any questions.\nIf you are only looking for naloxone/Narcan, please go to nextdistro.org/naloxone. Do not fill out this form.'
     },
     { // CA-enrollment-form
       formUUID: 'b48a73bb-d7ee-4cbc-9420-fbb8614bbab3',
       programUUID: '224ede70-0b7b-432c-89b1-8f4b938cfcad',
       stateCode: 'CA',
+      title: 'Example of a form title.',
+      instructions: 'Here go example instructions.'
+    },
+    { // NY-other-form
+      formUUID: 'b0b649c2-03c2-490f-89b4-fcd16b953986',
+      programUUID: '21154dc7-5fac-41f9-a058-a4ee492cb304',
+      stateCode: 'NY',
       title: 'Example of a form title.',
       instructions: 'Here go example instructions.'
     }
