c24da076ba094b76ab8e95cdd277f4078a1631b8
Bonga <bonga.developer@outlook.com>
2021-07-26 21:26:11 +0200

    password salting

--- server/config/db.js
+++ server/config/db.js
@@ -23,6 +23,6 @@ if (process.env.DATABASE_URL){
 }
 
 
-const db = new Sequelize(process.env.DATABASE_URL || `postgres://postgres:password@localhost:5432/`, config )
+const db = new Sequelize(process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, config )
 
 module.exports = db
