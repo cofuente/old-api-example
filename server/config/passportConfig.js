eb14b0f58cac4c34dcce2a098d4bd94b71c751e9
Bonga <bonga.developer@outlook.com>
2021-07-27 21:46:55 +0200

    add async/await style

--- server/config/passportConfig.js
+++ server/config/passportConfig.js
@@ -3,18 +3,21 @@ const User = require('../models').User
 
 module.exports = function(passport){
     passport.use(
-        new localStrategy( ( username, password, done ) => {
-            //Find if the user exists
-            User.findOne( {where: {username}} )
-            .then( user => {
-                if ( !user ){
-                    return done( null, false, {message: ' User doesnt exist'} )
-                } else if ( !user.correctPassword( password ) ){
+        new localStrategy( async ( username, password, done ) => {
+            try{
+                 //Find if the user exists
+                const user = await User.findOne( {where: {username}} )
+                    if ( !user ){
+                        return done( null, false, {message: ' User doesnt exist'} )
+                    }else if ( !user.correctPassword( password ) ){
                     // return if user password don't match
                     return done( null, false, {message: 'Wrong username and/or password'} )
-                }
-                return done( null, user )
-            })
+            }
+            return done( null, user )
+
+            }catch( err ){
+                console.log(err)
+            }
         })
     )
     passport.serializeUser(function( user, done ){
