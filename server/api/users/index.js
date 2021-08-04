dfb6cbeaea6b73ecde733c1a3b1f8dc9825c70d6
Chiara Marcial Martínez <contact@cofuente.io>
2021-08-04 19:26:33 -0400

    add all users route
    Signed-off-by: Chiara Marcial Martínez <contact@cofuente.io>

--- server/api/users/index.js
+++ server/api/users/index.js
@@ -1,4 +1,14 @@
 const router = require( 'express' ).Router()
+const User = require( '../../models/user' )
 
+// get all users
+router.get( '/', async ( req, res, next ) => {
+  try {
+    const users = await User.findAll()
+    res.json(users)
+  } catch (err) {
+    next(err)
+  }
+} )
 
 module.exports = router
