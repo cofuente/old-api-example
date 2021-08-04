04f91e000b3bb5acda9f576805e39eb0b921a919
Chiara Marcial Martínez <contact@cofuente.io>
2021-08-04 13:46:48 -0400

    refactor complete
    Signed-off-by: Chiara Marcial Martínez <contact@cofuente.io>

--- server/config/db.js
+++ server/config/db.js
@@ -1,34 +1,28 @@
 const Sequelize = require('sequelize')
-const pkg = require('../../package.json')
-
+const pkg = require( '../../package.json' )
+const CURRENT_ENV = process.env.CURRENT_ENV || 'LOCAL'
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+
+// on localhost
+const configForLocal = {
+  ssl: false,
+  logging: false,
+  dialect: 'postgres',
+}
 
 // on staging or prod
 const configForHosted = {
-  logging: false,
   ssl: true,
-  dialect: 'postgres',
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false // TODO: investigate switching to true
     }
-  },
-}
-// on localhost
-const configForLocal = {
-  ssl: false,
-  dialectOptions: {
-    ssl: {
-      require: false,
-      rejectUnauthorized: false
-    }
-  },
+  }
 }
 
 // replaces keys depending on db url presence
-const config = process.env.DATABASE_URL ? configForHosted : Object.assign(configForHosted, configForLocal)
-
+const config = CURRENT_ENV === 'LOCAL' ? configForLocal : Object.assign(configForLocal, configForHosted)
 
 const db = new Sequelize(process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, config )
 
