255e00757f667a73b1d7c7e5ba73d4b2bc003097
Bonga <bonga.developer@outlook.com>
2021-08-06 15:41:28 +0200

    db.js fix

--- server/config/db.js
+++ server/config/db.js
@@ -25,6 +25,6 @@ if (process.env.DATABASE_URL){
 }
 
 
-const db = new Sequelize(process.env.DATABASE_URL || `postgres://postgres:password@localhost:5432/secure_form_api`, config )
+const db = new Sequelize(process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, config )
 
 module.exports = db
