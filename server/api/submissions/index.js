efde8fe9461f09a9454b0835c81f9ed13c384956
Chiara Marcial Martínez <contact@cofuente.io>
2021-08-09 16:15:56 -0400

    created get all submissions endpoint
    Signed-off-by: Chiara Marcial Martínez <contact@cofuente.io>

--- server/api/submissions/index.js
+++ server/api/submissions/index.js
@@ -4,28 +4,16 @@ const Answer = require( '../../models' ).Answer
 
 // Get submission by FormID
 router.get( '/:formUUID', async ( req, res, next ) => {
-    try {
-      const {formUUID} = req.params
-      const submission = await Submission.findOne( {
-        where: {formUUID},
-        attributes: [ 'formUUID', 'submissionUUID']
-      } )
-      const SubmissionUUID = submission.submissionUUID
-      const answers = await Answer.findAll({
-          where: {
-            submissionUUID: SubmissionUUID
-          },
-          attributes: [ 'questionUUID', 'value']
-      }).then( (answersList => answersList.map(answer => answer)))
-      const responseObj = {
-          submission,
-          answers
-      }
-
-      res.json(responseObj)
-    } catch (err) {
-      next(err)
-    }
-  } )
+  try {
+    const {formUUID} = req.params
+    const submissions = await Submission.findAll( {
+      where: {formUUID},
+        include: [ Answer ]
+      })
+    res.json(submissions)
+  } catch (err) {
+    next(err)
+  }
+} )
 
 module.exports = router
