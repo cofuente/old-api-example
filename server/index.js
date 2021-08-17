00b03e9d66b4d266cfe1f45c810b2a309631f898
Chiara Marcial Martínez <contact@cofuente.io>
2021-08-17 15:48:36 -0400

    remove unused packages and variables
    Signed-off-by: Chiara Marcial Martínez <contact@cofuente.io>

--- server/index.js
+++ server/index.js
@@ -2,14 +2,9 @@ const express = require('express')
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
-// const session = require('express-session')
-// const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 const db = require( './config/db' )
-// const sessionStore = new SequelizeStore({db})
 const PORT = process.env.PORT || 1337
-const CURRENT_ENV = process.env.CURRENT_ENV || 'LOCAL'
-const CURRENT_DOMAIN = process.env.CURRENT_DOMAIN || 'http://localhost:1337'
 const server = express()
 
 // logging middleware
@@ -25,24 +20,6 @@ server.use( express.urlencoded( {extended: true} ) ) // TODO: investigate use if
 // typical express setup
 server.use(express.json())
 server.use(cors())
-
-// // saved sessions
-// server.use(
-//   session({
-//     secret: process.env.SESSION_SECRET || 'xXxX!!23@Abc',
-//     resave: false, // TODO: investigate use
-//     store: sessionStore,
-//     saveUninitialized: false,
-//     name: '24hrsessionId',
-//     cookie: {
-//       secure: true,
-//       httpOnly: CURRENT_ENV !== 'LOCAL',
-//       domain: CURRENT_DOMAIN,
-//       // path: 'foo/bar', // TODO: investigate this use
-//       expires: new Date(Date.now() + 60 * 60 * 1000) // set to 1 hour but pls add moment.js specifics later
-//     }
-//   })
-// )
 
 // routes
 server.use('/api', require('./api') )
