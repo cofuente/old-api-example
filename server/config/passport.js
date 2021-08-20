6ac6ea5a214de454c464cc327be0d3bfac256bc4
Chiara Marcial Martínez <contact@cofuente.io>
2021-08-20 12:14:22 -0400

    added comment
    Signed-off-by: Chiara Marcial Martínez <contact@cofuente.io>

--- server/config/passport.js
+++ server/config/passport.js
@@ -33,6 +33,8 @@ const strategy = new LocalStrategy(localVerification)
 
 // register strategy
 passport.use( strategy )
+
+// authorization middleware
 const isAuth = ( req, res, next ) => {
 	if ( req.isAuthenticated() ) return next()
 	else res.status( 401 ).json( {msg: 'You are not authorized to view this resource'} )
