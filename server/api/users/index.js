cb7b0dc815e33244d709a91591d5291b76ee3610
Chiara Marcial Martínez <contact@cofuente.io>
2021-08-19 23:25:03 -0400

    protect route
    Signed-off-by: Chiara Marcial Martínez <contact@cofuente.io>

--- server/api/users/index.js
+++ server/api/users/index.js
@@ -1,8 +1,11 @@
 const router = require( 'express' ).Router()
 const User = require( '../../models/user' )
+const {isAuth} = require( '../../config')
 
 // get all users
-router.get( '/', async ( req, res, next ) => {
+router.get( '/',
+  isAuth,
+  async ( req, res, next ) => {
   try {
     const users = await User.findAll()
     res.json(users)
